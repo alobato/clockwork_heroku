52a1cfb8750f58fa6420d50c5b28f1b9128895c8
Arley Lobato <arleylobato@gmail.com>
2013-04-04 15:21:38 -0300

    ok

--- clock.rb
+++ clock.rb
@@ -4,10 +4,12 @@ require 'open-uri'
 include Clockwork
 
 handler do |job|
+
   resposta = URI.parse('http://al-123board.herokuapp.com/metrics/5.txt').read
-  if resposta.to_i > 300
+  if resposta.to_i > 500
     Net::HTTP.post_form(URI.parse('http://api.prowlapp.com/publicapi/add'), { apikey: '9e6b86dec361a6c01080d481ba18974a2e38f4e4', application: 'lb', event: 'Resposta', description: resposta, priority: 0 }) rescue nil
   end
+
 end
 
 every(5.minutes, 'job')
