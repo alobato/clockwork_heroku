5f73fae7ca49c97004105ee63ada0faa7f3f936b
Arley Lobato <arleylobato@gmail.com>
2013-04-13 16:49:49 -0300

    ok

--- clock.rb
+++ clock.rb
@@ -19,10 +19,24 @@ handler do |job|
     end
   end
 
-  if job == 'ruby_ram'
-    ruby_ram = URI.parse('http://al-123board.herokuapp.com/metrics/6.txt').read.gsub("\n", '')
-    if ruby_ram.to_f > 140
-      Net::HTTP.post_form(URI.parse('http://api.prowlapp.com/publicapi/add'), { apikey: '9e6b86dec361a6c01080d481ba18974a2e38f4e4', application: 'lb', event: 'Ruby RAM', description: ruby_ram, priority: 0 }) rescue nil
+  if job == 'ruby_memory'
+    ruby_memory = URI.parse('http://al-123board.herokuapp.com/metrics/6.txt').read.gsub("\n", '')
+    if ruby_memory.to_f > 140
+      Net::HTTP.post_form(URI.parse('http://api.prowlapp.com/publicapi/add'), { apikey: '9e6b86dec361a6c01080d481ba18974a2e38f4e4', application: 'lb', event: 'ruby_memory', description: ruby_memory, priority: 0 }) rescue nil
+    end
+  end
+
+  if job == 'mysql_memory'
+    mysql_memory = URI.parse('http://al-123board.herokuapp.com/metrics/7.txt').read.gsub("\n", '')
+    if mysql_memory.to_f > 21
+      Net::HTTP.post_form(URI.parse('http://api.prowlapp.com/publicapi/add'), { apikey: '9e6b86dec361a6c01080d481ba18974a2e38f4e4', application: 'lb', event: 'mysql_memory', description: mysql_memory, priority: 0 }) rescue nil
+    end
+  end
+
+  if job == 'nginx_memory'
+    nginx_memory = URI.parse('http://al-123board.herokuapp.com/metrics/8.txt').read.gsub("\n", '')
+    if nginx_memory.to_f > 4
+      Net::HTTP.post_form(URI.parse('http://api.prowlapp.com/publicapi/add'), { apikey: '9e6b86dec361a6c01080d481ba18974a2e38f4e4', application: 'lb', event: 'nginx_memory', description: nginx_memory, priority: 0 }) rescue nil
     end
   end
 
@@ -30,4 +44,6 @@ end
 
 every(2.minutes, 'resposta')
 every(5.minutes, 'online')
-every(2.minute, 'ruby_ram')
+every(2.minutes, 'ruby_memory')
+every(5.minutes, 'mysql_memory')
+every(5.minutes, 'nginx_memory')
